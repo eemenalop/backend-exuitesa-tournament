b12cd50daa42f0ad7960000cfa450f86e0c0c054
eemenalop <eemena26@gmail.com>
2024-08-21 11:22:39 -0400

    add Get Teams Endpoints

--- netlify/functions/getTeams.js
+++ netlify/functions/getTeams.js
@@ -1,31 +1,29 @@
-// netlify/functions/getTeams.js
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = require('../../config.js')
 
 exports.handler = async (event) => {
-    try {
-      // Realiza una solicitud asíncrona para obtener los datos de la tabla 'teams'
-      const { data, error } = await supabase
-        .from('teams')
-        .select('*');
-  
-      // Si hay un error en la consulta, lanza una excepción
-      if (error) throw error;
-  
-      // Devuelve una respuesta exitosa con los datos obtenidos en formato JSON
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
-    } catch (error) {
-      // Si ocurre un error, devuelve una respuesta con el código de error 500
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: error.message }),
-      };
-    }
-  };
+  try {
+    // Realiza una solicitud asíncrona para obtener los datos de la tabla 'teams'
+    const { data, error } = await supabase
+      .from('teams')
+      .select('*');
+
+    // Si hay un error en la consulta, lanza una excepción
+    if (error) throw error;
+
+    // Devuelve una respuesta exitosa con los datos obtenidos en formato JSON
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  } catch (error) {
+    // Si ocurre un error, devuelve una respuesta con el código de error 500
+    console.error('Error fetching teams:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
