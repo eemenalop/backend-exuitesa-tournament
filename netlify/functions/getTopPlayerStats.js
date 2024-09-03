24ae03916b2a0c25481a86f2d601fdefaaba25eb
eemenalop <eemena26@gmail.com>
2024-09-03 16:44:13 -0400

    keep working with getTopPlayersStats endpoint

--- netlify/functions/getTopPlayerStats.js
+++ netlify/functions/getTopPlayerStats.js
@@ -3,17 +3,42 @@ const supabase = require('../../config.js')
 exports.handler = async () => {
 
     try {
-        const { data, error } = await supabase
-            .from('players_matches_stats')
-            .select('*')
+        const { data: poinstData, error: pointsError } = await supabase.rpc('calculate_points_per_game')
+            .limit(10);
+        if (pointsError) throw pointsError;
 
-        if (error) {
-            throw error
+        const { data: assistsData, error: assistsError } = await supabase.rpc('calculate_assists_per_game')
+            .limit(10);
+        if (assistsError) throw assistsError;
+
+        const { data: reboundsData, error: reboundsError } = await supabase.rpc('calculate_rebounds_per_game')
+            .limit(10);
+        if (reboundsError) throw reboundsError;
+
+        const { data: stealsData, error: stealsError } = await supabase.rpc('calculate_steals_per_game')
+            .limit(10);
+        if (stealsError) throw stealsError;
+
+        const { data: blocksData, error: blocksError } = await supabase.rpc('calculate_blocks_per_game')
+            .limit(10);
+        if (blocksError) throw blocksError;
+
+        const addRanking = (data) => data.map((item, index) => ({
+            rank: index + 1,
+            ...item
+        }));
+
+        const response = {
+            points: { data: addRanking(poinstData) },
+            assists: { data: addRanking(assistsData) },
+            rebounds: { data: addRanking(reboundsData) },
+            steals: { data: addRanking(stealsData) },
+            blocks: { data: addRanking(blocksData) }
         }
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            body: JSON.stringify(response.steals)
         }
 
     } catch (error) {
