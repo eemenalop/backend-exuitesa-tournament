cd7036e5f9ff0271aef7fb53070ade9aca429126
eemenalop <eemena26@gmail.com>
2024-09-13 00:31:40 -0400

    update getAllMatches endpoint to recibe param match_type (optional)

--- netlify/functions/getAllMatches.js
+++ netlify/functions/getAllMatches.js
@@ -2,12 +2,34 @@ const supabase = require("../../config");
 const { checkData } = require("./checkData");
 
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            body: ''
+        };
+    }
 
+    const matcheType = event.queryStringParameters.match_type;
     try {
-        const { data, error } = await supabase
+        let data;
+        let error;
+
+        if (matcheType) {
+        ({ data, error } = await supabase
             .from('matches')
-            .select('*');
+            .select('*')
+            .eq('match_type', matcheType));
+        } else {
+        ({ data, error } = await supabase
+            .from('matches')
+            .select('*'));
+        }
 
         if (error) throw error;
 
