62c1e34f487494fbc92c86e8df10b5fe77a73cfb
eemenalop <eemena26@gmail.com>
2024-09-18 23:24:00 -0400

    Add cors to createMatch ENDPOINT

--- netlify/functions/createMatch.js
+++ netlify/functions/createMatch.js
@@ -2,6 +2,17 @@ const supabase = require('../../config.js')
 
 
 exports.handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            body: JSON.stringify({})
+        };
+    }
 
     if (event.httpMethod !== 'POST') {
         return {
